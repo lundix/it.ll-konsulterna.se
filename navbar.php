61ea88f15d4b009a77661731cdd7f8d14c8c85e5
lundix <hanslossman@gmail.com>
2022-04-20 23:14:03 +0200

    My first commit

---
+++ navbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container px-5">
+      <img src="pic/logo90px.png" class="img-fluid" alt="Responsive image">
+        <a class="navbar-brand" href="index.html">LL Konsulterna - IT</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item"><a class="nav-link" href="index.html">Hem</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.html">Om</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Kontakt</a></li>
+                <li class="nav-item"><a class="nav-link" href="pricing.html">Priser</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownBlog" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Blog</a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownBlog">
+                        <li><a class="dropdown-item" href="blog-home.html">Min Blog</a></li>
+                        <li><a class="dropdown-item" href="blog-post.html">Senaste Post</a></li>
+                    </ul>
+</nav>
